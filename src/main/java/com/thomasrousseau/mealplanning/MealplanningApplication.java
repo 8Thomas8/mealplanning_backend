e60565610e5ec55959c98e01949d18bd3a6b59cc
Thomas Rousseau <thomas.rousseau@protonmail.com>
2020-04-02 12:42:09 +0200

    Initialisation du projet

---
+++ src/main/java/com/thomasrousseau/mealplanning/MealplanningApplication.java
@@ -0,0 +1,13 @@
+package com.thomasrousseau.mealplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealplanningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealplanningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thomasrousseau/mealplanning/MealplanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomasrousseau.mealplanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealplanningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
