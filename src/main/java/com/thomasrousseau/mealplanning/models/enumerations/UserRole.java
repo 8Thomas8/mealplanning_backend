f1901e4099b9455094dbdfe89e41e5ac55200c43
Thomas Rousseau <thomas.rousseau@protonmail.com>
2020-04-03 17:52:30 +0200

    Correction des énums

--- src/main/java/com/thomasrousseau/mealplanning/models/enumerations/UserRole.java
+++ src/main/java/com/thomasrousseau/mealplanning/models/enumerations/UserRole.java
@@ -1,8 +1,23 @@
 package com.thomasrousseau.mealplanning.models.enumerations;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
 /**
  * Enumeration of user's role authorized.
  */
+@RequiredArgsConstructor
+@Getter
 public enum UserRole {
-    ADMIN, USER
+    ADMIN(0, "Admin"),
+    USER(1, "User");
+
+    @NonNull
+    private final int value;
+
+    @NonNull
+    @JsonValue
+    private final String label;
 }
