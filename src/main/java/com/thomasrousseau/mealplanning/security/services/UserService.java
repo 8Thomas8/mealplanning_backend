be42569b83b04325127cde3f4e93fe6df5ecc087
Thomas Rousseau <thomas.rousseau@protonmail.com>
2020-04-10 18:29:35 +0200

    Ajout du UserService

---
+++ src/main/java/com/thomasrousseau/mealplanning/security/services/UserService.java
@@ -0,0 +1,36 @@
+package com.thomasrousseau.mealplanning.security.services;
+
+import com.thomasrousseau.mealplanning.database.repositories.UserRepository;
+import com.thomasrousseau.mealplanning.models.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+
+        Objects.requireNonNull(username);
+        User user = userRepository.findUserWithName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return user;
+    }
+
+    public Boolean comparePassword(String passwordAuth, String passwordBase) {
+        return passwordAuth.equals(passwordBase);
+    }
+}
