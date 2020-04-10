a7eb8d8b340211f87e45a8a8f218d51b2dce3961
Thomas Rousseau <thomas.rousseau@protonmail.com>
2020-04-10 18:28:31 +0200

    Modification userRepository

--- src/main/java/com/thomasrousseau/mealplanning/database/repositories/UserRepository.java
+++ src/main/java/com/thomasrousseau/mealplanning/database/repositories/UserRepository.java
@@ -2,6 +2,12 @@ package com.thomasrousseau.mealplanning.database.repositories;
 
 import com.thomasrousseau.mealplanning.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query(" SELECT u FROM User u "
+            + " WHERE u.username = ?1 ")
+    Optional<User> findUserWithName(String username);
 }
