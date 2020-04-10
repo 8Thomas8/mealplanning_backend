23d033a7dc6f3d131415e9208bb6b579a9fd59c8
Thomas Rousseau <thomas.rousseau@protonmail.com>
2020-04-10 18:28:16 +0200

    Modification entity User

--- src/main/java/com/thomasrousseau/mealplanning/models/User.java
+++ src/main/java/com/thomasrousseau/mealplanning/models/User.java
@@ -5,8 +5,11 @@ import com.thomasrousseau.mealplanning.database.contracts.UserContract;
 import com.thomasrousseau.mealplanning.models.base.EntityBase;
 import com.thomasrousseau.mealplanning.models.enumerations.UserRole;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Collection;
 
 /**
@@ -17,15 +20,16 @@ import java.util.Collection;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter @Setter
 @AttributeOverride(name="id", column = @Column(name = UserContract.COL_ID))
-public class User extends EntityBase {
+public class User extends EntityBase implements Serializable, UserDetails {
 
     /**
      * The name of the user.
      */
-    @JsonProperty(value = UserContract.COL_NAME)
-    @Column(name = UserContract.COL_NAME, nullable = false, length = 50, unique = true)
-    private String name;
+    @JsonProperty(value = UserContract.COL_USERNAME)
+    @Column(name = UserContract.COL_USERNAME, nullable = false, length = 50, unique = true)
+    private String username;
 
     /**
      * Email of the user.
@@ -52,7 +56,32 @@ public class User extends EntityBase {
     /**
      * Association with a Planning object.
      */
-    @OneToMany()
+    @OneToMany(fetch = FetchType.EAGER)
     @JsonProperty(value = "plannings")
     private Collection<Planning> planning;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
