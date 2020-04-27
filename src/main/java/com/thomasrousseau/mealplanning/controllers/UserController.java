f27d157c67fcfce3721ac8823e41830376532f1a
Thomas Rousseau <thomas.rousseau@protonmail.com>
2020-04-27 12:13:10 +0200

    Ajout de l'encodage lors de l'inscription

--- src/main/java/com/thomasrousseau/mealplanning/controllers/UserController.java
+++ src/main/java/com/thomasrousseau/mealplanning/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.thomasrousseau.mealplanning.controllers.base.BaseRestController;
 import com.thomasrousseau.mealplanning.database.repositories.UserRepository;
 import com.thomasrousseau.mealplanning.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/user")
 public class UserController extends BaseRestController<User, Integer> {
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     public UserController(@Autowired UserRepository repository) {
         super(repository);
     }
 
     @PostMapping("/register")
     public User register(@RequestBody User user) {
-        return this.save(user);
+        User account = new User();
+        account.setUsername(user.getUsername());
+        account.setEmail(user.getEmail());
+        account.setRole(user.getRole());
+        account.setPassword(passwordEncoder.encode(user.getPassword()));
+        return this.save(account);
     }
 }
