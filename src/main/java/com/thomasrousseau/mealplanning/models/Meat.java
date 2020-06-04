edccf12bb6e90377cff0cdc2816e2ed94a1d3527
Thomas Rousseau <thomas.rousseau@protonmail.com>
2020-06-04 14:14:24 +0200

    Suppression de l'héritage et modification des classes liées

--- src/main/java/com/thomasrousseau/mealplanning/models/Meat.java
+++ src/main/java/com/thomasrousseau/mealplanning/models/Meat.java
@@ -1,7 +1,9 @@
 package com.thomasrousseau.mealplanning.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.thomasrousseau.mealplanning.database.contracts.IngredientContract;
 import com.thomasrousseau.mealplanning.database.contracts.MeatContract;
+import com.thomasrousseau.mealplanning.models.base.EntityBase;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -17,7 +19,11 @@ import javax.persistence.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @AttributeOverride(name = "id", column = @Column(name = MeatContract.COL_ID))
-public class Meat extends Ingredient {
+public class Meat extends EntityBase {
+
+    @Column(name = IngredientContract.COL_NAME, nullable = false, unique = true)
+    @JsonProperty(value = IngredientContract.COL_NAME)
+    private String name;
 
     /**
      * The number of piece by person.
